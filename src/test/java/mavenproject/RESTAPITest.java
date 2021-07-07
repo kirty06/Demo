971bb6626d28de7ae86fc8cda1c5d38859d57421
kirty06 <kirty.kumari74@gmail.com>
2021-07-07 22:32:17 +0530

    first commit

---
+++ src/test/java/mavenproject/RESTAPITest.java
@@ -0,0 +1,17 @@
+package mavenproject;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("PostJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+}
